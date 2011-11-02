249465002c8447b251f29e7ae2a4e9cc496a04b1
Gareth Jones <gareth.e.jones@gmail.com>
2011-11-02 16:20:06 -0500

    bit friendlier for the terminalz
    Conflicts:
    
    	emacs.d/init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -24,10 +24,70 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
+;; ENVIRONMENT
+;;--------------------------------------------------
+
+;; fix the PATH variable
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(if window-system (set-exec-path-from-shell-PATH))
+
+;; dir to store all extra extensions
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+
+(setq vendor-dir (concat dotfiles-dir "/vendor"))
+
+;; add my vendor dir to load path
+(add-to-list 'load-path vendor-dir)
+
+;; CODING STYLES
+;;--------------------------------------------------
+
+;; smooth-scrolling stops that annoying jump when moving around
+(require 'smooth-scrolling)
+
+;; makes sexps flash when you eval them!
+(require 'highlight)
+(require 'eval-sexp-fu)
+
+;; use inconsolata
+(set-face-attribute 'default nil
+                    :family "Inconsolata"
+                    :height 160)
+
+;; show line numbers
+(when window-system (global-linum-mode t))
+
+;; tabs are 2 spaces
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+
 ;; solarized
-(require 'color-theme)
-(require 'color-theme-solarized)
-(color-theme-solarized-light)
+(when window-system
+  (require 'color-theme)
+  (eval-after-load 'color-theme
+    (progn
+      (require 'color-theme-solarized)
+      (color-theme-solarized-light))) )
+
+;; KEYBINDINGS
+;;--------------------------------------------------
+
+;; make option the super key on mac
+(setq mac-option-modifier 'super)
+
+;; map meta to command key for mac
+(setq ns-command-modifier 'meta)
+
+(global-set-key [f7] 'ns-toggle-fullscreen)
+
+;; steve yegges's suggested keybindings
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
 
 
 
